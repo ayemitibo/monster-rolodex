cc0b44b62397bf2159c27347971184fd1b318bdb
abiodun <abiodun@crenettechlabs.com>
2020-07-15 14:05:18 +0100

    Updates

---
+++ precache-manifest.4630bb7a07c6fd31f5c17bea6e89befc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d47f7f5146368fb37b32295e6eb1652",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "33a1b21a5bbc1bba8928",
+    "url": "/monster-rolodex/static/css/main.9e3312c7.chunk.css"
+  },
+  {
+    "revision": "b150ae01874231adcaea",
+    "url": "/monster-rolodex/static/js/2.c60fa162.chunk.js"
+  },
+  {
+    "revision": "33a1b21a5bbc1bba8928",
+    "url": "/monster-rolodex/static/js/main.c4f0bf3f.chunk.js"
+  },
+  {
+    "revision": "7baeefba2a7c44825d28",
+    "url": "/monster-rolodex/static/js/runtime-main.c750203f.js"
+  }
+]);
